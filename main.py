d025d375e629c0ea8c6f3ce999d1138b8121bae5
kulkarnisankalp598-png <kulkarnisankalp598@gmail.com>
2025-12-27 19:29:12 -0600

    Update GPA calculator with multiple course input

--- main.py
+++ main.py
@@ -5,3 +5,27 @@ grade_points= {
     "D": 1.0,
     "F": 0.0
 }
+courses= {}
+def add_course():
+    name= input("Enter Course Name:")
+    grade= input("Enter Letter Grade A-F: ").upper()
+    credit= int(input("How many credits is the class?: "))
+    courses[name]={"grade":grade, "credits":credit}
+def calc_gpa():
+    total_points=0
+    total_credits=0
+    for lebron in courses.values():
+        grade= lebron["grade"]
+        credit=lebron["credits"]
+        total_points += grade_points[grade] * credit
+        total_credits += credit
+    if total_credits==0:
+        return 0
+    return total_points / total_credits
+while True:
+    add_course()
+    more = input("Add another course? (y/n): ").lower()
+    if more != "y":
+        break
+gpa=calc_gpa
+print(f"Your gpa is: {gpa:.2f}")
